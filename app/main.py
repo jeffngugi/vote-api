2270a3f356b55e66c6817b97d901014773041c02
jeffngugi <ngugimwathi254@gmail.com>
2024-11-25 16:51:08 +0300

    ch: add cors midleware"

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .database import engine
 from . import models
 from .routers import post, user, auth, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
@@ -12,6 +13,16 @@ print(settings.database_password)
 
 app = FastAPI() 
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
